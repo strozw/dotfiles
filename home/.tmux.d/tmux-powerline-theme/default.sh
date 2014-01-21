d024bbe0c25f146b02208176b49e0d813d65661b
satoru ohzawa <satoru.ohzawa@gmail.com>
2014-01-21 17:14:20 +0900

    =update

--- home/.tmux.d/tmux-powerline-theme/default.sh
+++ home/.tmux.d/tmux-powerline-theme/default.sh
@@ -12,6 +12,7 @@ else
 	TMUX_POWERLINE_SEPARATOR_RIGHT_THIN="❯"
 fi
 
+
 TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR:-'235'}
 TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-'255'}
 
@@ -41,12 +42,12 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
 		"pwd 89 211" \
-		"mailcount 9 255" \
-		"now_playing 234 37" \
+		#"mailcount 9 255" \
+		#"now_playing 234 37" \
 		#"cpu 240 136" \
 		"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
+		#"battery 137 127" \
 		"weather 37 255" \
 		#"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
