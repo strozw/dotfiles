c11731804873e590711e47a4a91fc95414ca981d
strozw <strozw@users.noreply.github.com>
2022-09-07 15:48:58 +0900

    update

--- private_dot_config/nvim/lua/plugins.lua
+++ private_dot_config/nvim/lua/plugins.lua
@@ -72,11 +72,45 @@ packer.startup(function(use)
 		end
 	}
 	use {
-		'folke/tokyonight.nvim',
+		'xiyaowong/nvim-transparent',
 		config = function()
-			vim.g.tokyonight_style = "night"
-			vim.g.tokyonight_italic_functions = true
+			local transparent = require("transparent")
+
+			transparent.setup({})
+		end
+	}
+	use {
+		'folke/tokyonight.nvim',
+		requires = {
+			'xiyaowong/nvim-transparent',
+		},
+		config = function()
+			require("tokyonight").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+				transparent = false, -- Enable this to disable setting the background color
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value `:help attr-list`
+					comments = "italic",
+					keywords = "italic",
+					functions = "NONE",
+					variables = "NONE",
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "dark", -- style for sidebars, see below
+					floats = "dark", -- style for floating windows
+				},
+				sidebars = { "qf", "help", "Fern", "Telescope" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+				day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+				hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+				dim_inactive = false, -- dims inactive windows
+				lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+			})
+
 			vim.cmd [[colorscheme tokyonight]]
+			vim.g.tokyonight_transparent = vim.g.transparent_enabled
 		end
 	}
 	use {
@@ -759,7 +793,23 @@ packer.startup(function(use)
 	use { 'tpope/vim-fugitive' }
 	-- github extenstions for fugitive
 	use { 'tpope/vim-rhubarb' }
-	use { 'TimUntersberger/neogit' }
+	use {
+		'TimUntersberger/neogit',
+		config = function()
+
+			local neogit = require("neogit")
+
+
+			if neogit then
+				neogit.setup {
+					disable_builtin_notifications = false,
+					commit_popup = {
+						kind = "split",
+					},
+				}
+			end
+		end
+	}
 	use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 	use {
 		'akinsho/git-conflict.nvim',
--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -7,13 +7,28 @@ return {
 	font = wezterm.font("UDEV Gothic NF"),
 	use_ime = true,
 	font_size = 16.0,
+	line_height = 1.05,
 	color_scheme = "wezterm_tokyonight_night",
 	colors = {
 		tab_bar = {
-			-- The color of the inactive tab bar edge/divider
-			inactive_tab_edge = '#575757',
+			active_tab = {
+				bg_color = '#1a1b26',
+				fg_color = '#c0c0c0',
+			},
+
+			inactive_tab = {
+				bg_color = '#0f0f14',
+				fg_color = '#808080',
+			},
+
+			inactive_tab_hover = {
+				bg_color = '#24283b',
+				fg_color = '#909090',
+				italic = true,
+			},
 		},
 	},
+	window_background_opacity = 0.85,
 	window_padding = {
 		left = 0,
 		right = 0,
@@ -22,73 +37,11 @@ return {
 	},
 	window_decorations = "RESIZE",
 	window_frame = {
-		inactive_titlebar_bg = '#353535',
-		active_titlebar_bg = '#000',
-		inactive_titlebar_fg = '#cccccc',
-		active_titlebar_fg = '#ffffff',
-		inactive_titlebar_border_bottom = '#000',
-		active_titlebar_border_bottom = '#000',
-		button_fg = '#cccccc',
-		button_bg = '#000',
-		button_hover_fg = '#ffffff',
-		button_hover_bg = '#3b3052',
+		active_titlebar_bg = '#0f0f14',
 	},
 	hide_tab_bar_if_only_one_tab = true,
 	adjust_window_size_when_changing_font_size = false,
-	hyperlink_rules = {
-		-- Linkify things that look like URLs and the host has a TLD name.
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
-			format = '$0',
-		},
-
-		-- linkify email addresses
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-			format = 'mailto:$0',
-		},
-
-		-- file:// URI
-		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
-		{
-			regex = [[\bfile://\S*\b]],
-			format = '$0',
-		},
-
-		-- Linkify things that look like URLs with numeric addresses as hosts.
-		-- E.g. http://127.0.0.1:8000 for a local development server,
-		-- or http://192.168.1.1 for the web interface of many routers.
-		{
-			regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-			format = '$0',
-		},
-
-		-- Make task numbers clickable
-		-- The first matched regex group is captured in $1.
-		{
-			regex = [[\b[tT](\d+)\b]],
-			format = 'https://example.com/tasks/?t=$1',
-		},
-
-		-- Make username/project paths clickable. This implies paths like the following are for GitHub.
-		-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
-		-- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-		-- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-		{
-			regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-			format = 'https://www.github.com/$1/$3',
-		},
-	},
-	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
-		{
-			event = { Up = { streak = 1, button = 'Left' } },
-			mods = 'CTRL',
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
-	},
+	bypass_mouse_reporting_modifiers = 'ALT',
 
 	-- keys = {
 	-- 	-- {key="-", mods="CTRL", action="Nop"},
