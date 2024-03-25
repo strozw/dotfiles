910193b8528539b4256d8b16bdcd7140e01ebb89
strozw <strozw@users.noreply.github.com>
2024-03-25 09:15:18 +0900

    update

--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -188,9 +188,12 @@ return {
 	{
 		"antosha417/nvim-lsp-file-operations",
 		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{ "nvim-tree/nvim-tree.lua" },
+			"nvim-lua/plenary.nvim",
+			"nvim-neo-tree/neo-tree.nvim",
 		},
+		config = function()
+			require("lsp-file-operations").setup()
+		end,
 	},
 
 	-- {
