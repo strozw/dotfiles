e99ba54870b3102c113155aa11e1a2aa029457df
strozw <strozw@users.noreply.github.com>
2024-09-20 12:02:42 +0900

    update

--- home/.config/nvim/lua/plugins/markdown.lua
+++ home/.config/nvim/lua/plugins/markdown.lua
@@ -27,4 +27,13 @@ return {
 			vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 		end,
 	},
+
+	{
+		"dawsers/edit-code-block.nvim",
+		config = function()
+			require('ecb').setup {
+				wincmd = 'split', -- this is the default way to open the code block window
+			}
+		end
+	}
 }
--- home/.config/nvim/lua/plugins/ui.lua
+++ home/.config/nvim/lua/plugins/ui.lua
@@ -216,7 +216,7 @@ return {
 				},
 				{ view = "mini", filter = { event = "msg_show", kind = "", find = "written", }, },
 				{ view = "mini", filter = { event = "msg_show", kind = "", find = "書込み", }, },
-				{ view = "mini", filter = { event = "msg_show", kind = "emsg", find = "パターンは見つかりませんでした", }, },
+				{ view = "mini", filter = { event = "msg_show", kind = "emsg", find = "", }, },
 			},
 			---@type table<string, NoiceFilter>
 			status = {}, --- @see section on statusline components
@@ -381,14 +381,16 @@ return {
 								hide_filename_extension = false, -- Hide filename extension when set to true.
 								show_modified_status = true, -- Shows indicator when the buffer is modified.
 
-								mode = 0,                -- 0: Shows buffer name
+								mode = 0,
+								-- 0: Shows buffer name
 								-- 1: Shows buffer index
 								-- 2: Shows buffer name + buffer index
 								-- 3: Shows buffer number
 								-- 4: Shows buffer name + buffer number
 
 								-- max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-								max_length = vim.o.columns * 4 / 5, -- Maximum width of buffers component,
+								-- max_length = vim.o.columns * 4 / 5, -- Maximum width of buffers component,
+
 								-- it can also be a function that returns
 								-- the value of `max_length` dynamically.
 								filetype_names = {
