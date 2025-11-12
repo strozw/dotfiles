bcaab2ec5aaf0d726edc277ca3791db01b22befe
Satoru Ohzawa <s_ohzawa@yumemi.co.jp>
2025-11-12 21:10:38 +0900

    add autocmd for chezmoi file watching

--- dot_config/nvim/lua/plugins/chezmoi.lua
+++ dot_config/nvim/lua/plugins/chezmoi.lua
@@ -4,8 +4,18 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require("chezmoi").setup {
-        -- your configurations
       }
+
+      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+        pattern = { os.getenv("HOME") .. "/.local/share/chezmoi/*" },
+        callback = function(ev)
+          local bufnr = ev.buf
+          local edit_watch = function()
+            require("chezmoi.commands.__edit").watch(bufnr)
+          end
+          vim.schedule(edit_watch)
+        end,
+      })
     end
   }
 }
