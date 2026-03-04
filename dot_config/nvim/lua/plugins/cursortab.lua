2f2a06f9caf6e45b0c4a220512e2bbf9b4a1b571
strozw <strozw@users.noreply.github.com>
2026-03-04 23:24:16 +0900

    update

---
+++ dot_config/nvim/lua/plugins/cursortab.lua
@@ -0,0 +1,14 @@
+return {
+  -- "leonardcser/cursortab.nvim",
+  -- -- version = "*",  -- Use latest tagged version for more stability
+  -- build = "cd server && go build",
+  -- config = function()
+  --   require("cursortab").setup({
+  --     provider = {
+  --       type = "sweep",
+  --       url = "http://localhost:11434",
+  --       model = "sweepai/sweep-next-edit:latest",
+  --     },
+  --   })
+  -- end,
+}
