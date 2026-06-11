54537ea383bcd6b7c0467ee9ab52c3c33ff67727
strozw <strozw@users.noreply.github.com>
2026-06-11 09:05:01 +0900

    update nvim config

--- dot_config/nvim/lua/plugins/react-compiler-marker.lua
+++ dot_config/nvim/lua/plugins/react-compiler-marker.lua
@@ -3,4 +3,7 @@ return {
   'blazejkustra/react-compiler-marker',
   ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
   build = './scripts/build-nvim.sh',
+  config = function()
+    require('react-compiler-marker').setup({})
+  end
 }
